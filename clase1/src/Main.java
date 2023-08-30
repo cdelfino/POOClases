dda8308b642d3f250ecab9187bb8e11e06f00cf1
Constanza Delfino <111506427+cdelfino@users.noreply.github.com>
2023-08-30 15:21:39 -0300

    clase1

--- clase1/src/Main.java
+++ clase1/src/Main.java
@@ -1,46 +1,62 @@
-class Cat {
+package clase1.src;
+
+abstract class Cat  {
     private int age;
     private int color;
+    private int energy;
 
-    // Constructor para inicializar un gato con edad y color
-    public Cat(int age, int color) {
-        this.age = age;
-        this.color = color;
-    }
-
-    // Getter para obtener la edad
     public int getAge() {
         return age;
     }
 
-    // Setter para establecer la edad
     public void setAge(int age) {
         this.age = age;
     }
 
-    // Getter para obtener el color
     public int getColor() {
         return color;
     }
 
-    // Setter para establecer el color
     public void setColor(int color) {
         this.color = color;
+    }
+
+    public abstract String jump();
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
+}
+
+class SilentCat extends Cat {
+    public String jump() {
+        if(getEnergy() >= 10) return "Tired";
+        setEnergy(getEnergy() + 1);
+        return "....";
+    }
+}
+
+class WildCat extends Cat {
+    public String jump() {
+        if(getEnergy() >= 10) return "Tired";
+        setEnergy(getEnergy() + 5);
+        return "rrrrrr";
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        // Crear un nuevo gato
-        Cat miMichi = new Cat(6, 1);
-
-        // Imprimir la edad del gato
-        System.out.println("Edad de miMichi: " + miMichi.getAge());
-
-        // Cambiar la edad del gato
-        miMichi.setAge(7);
-
-        // Imprimir la nueva edad del gato
-        System.out.println("Nueva edad de miMichi: " + miMichi.getAge());
+        SilentCat miMichi = new SilentCat();
+        miMichi.setEnergy(1);
+        miMichi.setAge(6);
+        miMichi.setColor(1);
+        System.out.println(miMichi.jump());
+        System.out.println(miMichi.jump());
+        System.out.println(miMichi.jump());
+        System.out.println(miMichi.jump());
     }
 }
